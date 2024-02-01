4f3201202bec292a7ecf86372c067aa5a473723a
rebs-code <jack.rebussi@gmail.com>
2024-02-01 17:21:27 +0100

    feat: show comics title in view

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,1 +1,20 @@
 @extends('layouts.app')
+
+@section('main')
+    <div class="main-container">
+        <div class="container text-center">
+            <div>
+                <h3>Comics List</h3>
+            </div>
+        </div>
+        <div class="bottom-container">
+            <div class="container comics-container">
+                @foreach ($comics as $comic)
+                    <div class="comic">
+                        {{-- <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}"> --}}
+                        <h5 class="text-uppercase text-center">{{ $comic['title'] }}</h5>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    @endsection
