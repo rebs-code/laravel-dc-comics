796ce74618983bec7882e9a623b910c6ec452f14
rebs-code <jack.rebussi@gmail.com>
2024-02-05 18:02:34 +0100

    Refactor form inputs in edit.blade.php

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -21,51 +21,60 @@
         <form action="{{ route('comics.update', $comic->id) }}" method="POST">
             @csrf
             @method('PUT')
+            {{-- title input --}}
             <div class="form-group">
                 <label for="exampleInputEmail1">Title</label>
                 <input type="text" class="form-control" name="title" placeholder="Enter title"
-                    value="{{ $comic->title }}">
+                    value="{{ old('title', $comic->title) }}">
             </div>
+            {{-- description input --}}
             <div class="mb-3">
                 <label for="exampleFormControlTextarea1" class="form-label">Description</label>
-                <textarea class="form-control" name="description" rows="3">{{ $comic->description }}</textarea>
+                <textarea class="form-control" name="description" rows="3">{{ old('description', $comic->description) }}"</textarea>
             </div>
+            {{-- price input --}}
             <div class="form-group">
                 <label for="exampleInputEmail1">Price</label>
                 <input type="text" class="form-control" placeholder="Enter price" name="price"
-                    value="{{ $comic->price }}">
+                    value="{{ old('price', $comic->price) }}">
             </div>
+            {{-- series field --}}
             <div class="form-group">
                 <label for="exampleInputEmail1">Series</label>
                 <input type="text" class="form-control" placeholder="Enter Series" name="series"
-                    value="{{ $comic->series }}">
+                    value="{{ old('series', $comic->series) }}">
             </div>
+            {{-- date field --}}
             <div class="form-group">
                 <label for="exampleInputEmail1">
                     Date
                 </label>
                 <input type="text" class="form-control" placeholder="Enter date" name="sale_date"
-                    value="{{ $comic->sale_date }}">
+                    value="{{ old('sale_date', $comic->sale_date) }}">
             </div>
+            {{-- type field --}}
             <div class="form-group">
                 <label for="exampleInputEmail1">Type</label>
                 <input type="text" class="form-control" placeholder="Enter type" name="type"
-                    value="{{ $comic->type }}">
+                    value="{{ old('type', $comic->type) }}">
             </div>
+            {{-- artists field --}}
             <div class="form-group">
                 <label for="exampleInputEmail1">Artists</label>
                 <input type="text" class="form-control" placeholder="Enter artists separated by comma" name="artists"
-                    value="{{ $comic->artists }}">
+                    value="{{ old('artists', $comic->artists) }}">
             </div>
+            {{-- writers field --}}
             <div class="form-group">
                 <label for="exampleInputEmail1">Writers</label>
                 <input type="text" class="form-control" placeholder="Enter Writers separated by comma" name="writers"
-                    value="{{ $comic->writers }}">
+                    value="{{ old('writers', $comic->writers) }}">
             </div>
+            {{-- thumb field --}}
             <div class="form-group">
                 <label for="exampleInputEmail1">Thumb</label>
                 <input type="text" class="form-control" placeholder="Enter thumb url" name="thumb"
-                    value="{{ $comic->thumb }}">
+                    value="{{ old('thumb', $comic->thumb) }}">
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
