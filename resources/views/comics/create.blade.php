d51c8165feea1736ec6edf06b73db45ac1ed48eb
rebs-code <jack.rebussi@gmail.com>
2024-02-05 16:22:52 +0100

    add: code to display errors in create.blade.php at the top of the form

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -5,6 +5,18 @@
         <div>
             <h2>Insert a new Comic Book Record</h2>
         </div>
+
+        {{-- code to display the error messages --}}
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
         <form action="{{ route('comics.store') }}" method="POST">
             @csrf
             <div class="form-group">
